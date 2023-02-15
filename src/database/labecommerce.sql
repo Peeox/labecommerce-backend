f4c84752a0657cd26047547c3021efa3f06db2f4
Peeox <pedrofelippe644@gmail.com>
2023-02-15 14:12:06 -0300

    aprofundamento-sql

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -1,4 +1,4 @@
--- Active: 1676471908506@@127.0.0.1@3306
+-- Active: 1676479023870@@127.0.0.1@3306
 
 -- Criei o labecommerce.db e conectei ele no SQLite
 -- Criei o arquivo labecommerce.sql e iniciei o projeto aqui
@@ -17,13 +17,11 @@ VALUES
     (2, "devizinho@email.com", "devera123"),
     (3, "emailzinho@email.com", "cadeOEmail123");
 
--- Verifica as informações da tabela
-
-SELECT * FROM users;
-
 -- Verifica estrutura da tabela
 
 PRAGMA table_info('users');
+
+-- Cria tabela chamada products
 
 CREATE TABLE products (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
@@ -31,6 +29,8 @@ CREATE TABLE products (
     price REAL NOT NULL,
     category TEXT NOT NULL
 );
+
+-- Popula tabela
 
 INSERT INTO products (id, name, price, category)
     VALUES
@@ -40,4 +40,80 @@ INSERT INTO products (id, name, price, category)
     (4, "Laranja", 6, "hortifruti"),
     (5, "Pêssego", 9, "hortifruti");
 
-SELECT * FROM products;
+
+--  Exercício 1
+
+-- Get all users - Verifica as info da tabela users
+
+-- SELECT * FROM users;
+
+-- Get all users - Verifica as info da tabela products
+
+-- SELECT * FROM products;
+
+-- Search Product by name - Procura produto pelo nome na tabela
+
+SELECT * FROM products
+WHERE name = "Uva";
+
+-- Create User - Cria novo usuário
+
+INSERT INTO users (id, email, password)
+VALUES 
+    (4, "xablau@email.com", "ualbax123");
+
+-- Create Product -- Cria novo produto
+
+INSERT INTO products (id, name, price, category)
+VALUES
+    (6, "Manga", 8, "hortifruti");
+
+--  Exercício 2
+
+-- Get Product by ID
+
+SELECT * FROM products
+WHERE id = 4;
+
+-- Delete User by ID
+
+DELETE FROM users
+WHERE id = 1;
+
+-- Delete Product by ID
+
+DELETE FROM products
+WHERE id = 2;
+
+-- Edit user by ID
+
+UPDATE users
+SET email = "dev@devinho.com", password ="minion123"
+WHERE id = 2;
+
+-- Edit products by ID
+
+UPDATE products
+SET name = "Jaboticaba"
+WHERE id = 5;
+
+--  Exercício 3 - Refatore as queries abaixo:
+--      SELECT * FROM users;
+--      SELECT * FROM products;
+
+-- Get all users
+
+SELECT * FROM users
+    ORDER BY email ASC;
+
+-- Get all products versão 1
+
+SELECT * FROM products
+    ORDER BY price ASC
+    LIMIT 20;
+
+-- Get all products versão 2
+
+SELECT * FROM products
+WHERE price >= 1 AND price <= 8
+ORDER BY price ASC;
